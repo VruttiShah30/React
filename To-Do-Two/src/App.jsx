4e3f05ed2bd9393989adb0995f88eb4c0d8a1185
Vrutti Shah <vruttishah8@gmail.com>
2026-05-01 09:16:33 +0530

    auto-commit

--- To-Do-Two/src/App.jsx
+++ To-Do-Two/src/App.jsx
@@ -1,15 +1,27 @@
+import {useState} from "react";
 import { Create } from "./components/Create";
 import { Display } from "./components/Display";
 
 
 
 const App = () => {
-
+    var[task,setTask] = useState([]);   
+    const addtask = (data)=>{
+        var newarray = [...task,data];
+        setTask(newarray);
+    }
     
+
+    const removeTask = (data) =>{
+        var newarray = task.filter(ele =>{
+            return ele != data;
+        })
+        setTask(newarray);
+    }
     return (<>
         <h1>TO DO APP</h1>
-        <Create />
-        <Display />
+        <Create addtask={addtask}/>
+        <Display task={task} removeTask={removeTask}/>
     </>)
 
 }
