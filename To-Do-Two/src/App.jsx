f41b3573ae528aa2493f45f53289909785cccbeb
Vrutti Shah <vruttishah8@gmail.com>
2026-05-06 09:20:52 +0530

    ALL

--- To-Do-Two/src/App.jsx
+++ To-Do-Two/src/App.jsx
@@ -24,6 +24,7 @@ const App = () => {
         <div className="row">
             <div className="col-5 mx-auto card mt-5 p-5">
                 <h1 align='center'>TO DO APP</h1>
+                
                 <Create addtask={addtask}/>
                 <Display task={task} removeTask={removeTask}/>
             </div> 
